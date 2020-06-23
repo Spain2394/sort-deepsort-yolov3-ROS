211dc7bad282bb348e4fc1cc40217790f34d6332
Spain2394 <aspain2394@gmail.com>
2020-06-22 22:47:47 -0400

    detecting platns but there is still some error in detections

--- sort_track/src/bounding_boxes.py
+++ sort_track/src/bounding_boxes.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-import numpy
-import tf
+import numpy as np
+# import tf
 
 from os.path import expanduser
 from cv_bridge import CvBridge, CvBridgeError
 from darknet_ros_msgs.msg import BoundingBoxes
 from sensor_msgs.msg import Image
 from std_msgs.msg import Int8
+import cv2
 
 def getParam():
     image_topic = rospy.get_param("~camera_topic")
     detection_topic = rospy.get_param("~detection_topic")
+    display = rospy.get_param("~display")
     # object_topic = rospy.get_param("~object_topic")
 	# tracker_topic = rospy.get_param('~tracker_topic')
 	# cost_threhold = rospy.get_param('~cost_threhold')
@@ -24,7 +26,7 @@ def getParam():
 	# display = rospy.get_param("~display")
 	# fps = rospy.get_param("/video_stream_opencv/fps", 30)
 
-    return image_topic, detection_topic
+    return image_topic, detection_topic, display
 
 
 def image_callback(image):
@@ -47,64 +49,90 @@ def detector_callback(bbox):
     _detected_bbox = bbox
 
 
+def draw_detections(box, class_id, im):
+
+    global marked_image
+    marked_image = im
+
+    cv2.rectangle(marked_image,(box[0],box[1]),(box[2], box[3]), (0,255,0),2)
+    cv2.putText(marked_image, class_id, (box[0] - 10, box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
+
+
+
 def run():
 
+    # declare global variables that can be updated globally
     global _current_image
     global _bridge
     global _detected_bbox
-    # global _current_objects
-    # global coordinates
+    global coordinates
     global object_class
+    global dets
+    global marked_image
 
+
+    # initilize current image and detected boxes
+    marked_image = None
     _current_image = None
     _detected_bbox = None
-    # _current_objects = None
-    # create detector
+
+    # setup cv2 bridge 
     _bridge = CvBridge()
 
     # image and point cloud subscribers
-    image_topic, detection_topic = getParam()
+    image_topic, detection_topic, display = getParam()
 
     # and variables that will hold their values
-    rospy.Subscriber(image_topic, Image, image_callback)
+    rospy.Subscriber(image_topic, Image, image_callback, queue_size=10)
 
-    rospy.Subscriber(detection_topic, BoundingBoxes, detector_callback)
-
-    # rospy.Subscriber(object_topic, Int8, object_callback)
+    rospy.Subscriber(detection_topic, BoundingBoxes, detector_callback, queue_size=10)
 
     # publisher for frames with detected objects
     _imagepub = rospy.Publisher('~labeled_image', Image, queue_size=10)
 
     # TODO publish marked image with sort detections
 
-
     rospy.loginfo("ready to detect")
 
     while not rospy.is_shutdown():
+        boxes = []
+        class_ids = []
+        detections = 0
             # only run if there is an image
         if _current_image is not None:
             rospy.loginfo("current image received")
             try:
                 # convert image from the subscriber into an OpenCV image
-                scene = _bridge.imgmsg_to_cv2(_current_image, 'rgb8')
-                # publish marked images
-                rospy.loginfo("publishing")
-                _imagepub.publish(_bridge.cv2_to_imgmsg(scene, 'rgb8'))  # publish detection results
+                # initialize the detected image as the current frame
+                marked_image = _bridge.imgmsg_to_cv2(_current_image, 'rgb8')
+                # marked_image, objects = self._detector.from_image(scene)  # detect objects
+                
+                # _imagepub.publish(_bridge.cv2_to_imgmsg(scene, 'rgb8'))  # publish detection results
 
                 # what if there is no bounding boxes? 
+                # go through all the detections in each frame
                 if _detected_bbox is not None:
-                    for box, in _detected_bbox.bounding_boxes:
+                    for box in _detected_bbox.bounding_boxes:
                         xmin, ymin, xmax, ymax = box.xmin, box.ymin, box.xmax, box.ymax
-                        coordinates = np.array([xmin, ymin, xmax, ymax])
                         obj_class = box.Class
 
-                        rospy.loginfo('...' + str(obj_class)+ ' ' + str() + ' at ' + str(coordinates))
-                        # 0 for weed 1 for plant 
-                        if object_class == 1: 
-                            rospy.loginfo("object is a plant, pass bounding box to sort algorithm")
-                        # 1. publish image stream to topic labeled_images
-                        # 2. draw bounding boxes
+                        # rospy.loginfo(' ' + str(obj_class)+ ' at ' + str(dets))
+        
+                        rospy.loginfo(obj_class == "plant")
+                        if obj_class == "plant": 
+                            boxes.append([xmin, ymin, xmax, ymax])
+                            class_ids.append(obj_class)
+                            detections += 1
+                        else: pass  # no plants in the image
+                        rospy.loginfo("object is a plant, pass bounding box to sort algorithm")
 
+                for i in range(detections):
+                    draw_detections(boxes[i], class_ids[i], marked_image)
+                    # cv2.rectangle(marked_image,(100,100),(300, 300), (0,255,0),2)
+                    # cv2.putText(marked_image, "plant", (100 - 10, 100 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,255,0), 2)
+            
+                _imagepub.publish(_bridge.cv2_to_imgmsg(marked_image, 'rgb8'))  # publish detection results
+                    
             except CvBridgeError as e:
                 print(e)
 
